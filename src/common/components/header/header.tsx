b345e2c79447cf1a2f3c1e57f40879f73995a5f8
pedro-novo <pedro.novo.93@gmail.com>
2023-05-28 20:29:36 +0100

    add basic header structure

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@ui/avatar";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
-  return <div className="w-full h-16 bg-slate-200">Header</div>;
+  return (
+    <div className="w-full h-20 px-4 flex items-center justify-between sticky top-0 bg-gray-100 border-b border-gray-200">
+      <Link to="/" className="text-2xl">
+        LOGO
+      </Link>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  );
 };
 
 export default Header;
