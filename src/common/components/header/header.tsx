633e98e88e1fc4a000ea0877ce7a20be815c862f
pedro-novo <pedro.novo.93@gmail.com>
2023-05-30 18:21:42 +0100

    small style fix

--- src/common/components/header/header.tsx
+++ src/common/components/header/header.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <div className="w-full h-20 px-4 flex items-center justify-between sticky top-0 bg-gray-100 border-b border-gray-200">
+    <div className="w-full h-20 px-10 flex items-center justify-between sticky top-0 bg-gray-100 border-b border-gray-200">
       <Link to="/" className="text-2xl">
         LOGO
       </Link>
